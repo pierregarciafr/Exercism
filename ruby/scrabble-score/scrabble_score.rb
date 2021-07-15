ede3db51d5934a68d5ea82c7bc04f0f5d3f2cf94
pierre garcia <pg.pierre.garcia@free.fr>
2021-07-15 21:29:23 +0200

    increasing readability of the names of variables and methods

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,5 +1,5 @@
 class Scrabble
-  TILE = { A: 1, N: 1,
+  TILES = { A: 1, N: 1,
            B: 3, O: 1,
            C: 3, P: 3,
            D: 2, Q: 10,
@@ -19,18 +19,21 @@ class Scrabble
 
   private
 
-  def initialize(letters_set, tile_set = TILE)
-    @tile_set = tile_set
-    @tiles = normalize(letters_set)
-    @score = @tiles.collect { |tile_count| tile_count[0][1] * tile_count[1] }.sum
+  def initialize(letters_set, tiles = TILES)
+    @letters_set = letters_set.to_s.upcase.scan(/[A-Z]/).sort!
+    @tiles = tiles
+    @tiles_count = normalize
+    @score = compute_score
   end
 
-  def normalize(word)
-    valid_letters = word.to_s.upcase.scan(/[A-Z]/).sort!
-
-    @tile_set.each_with_object({}) do |tile, h|
-      h[tile] = valid_letters.include?(tile[0].to_s) ? valid_letters.count { |x| x == tile[0].to_s } : 0
+  def normalize
+    @tiles.each_with_object({}) do |tile, h|
+      h[tile] = @letters_set.include?(tile[0].to_s) ? @letters_set.count { |x| x == tile[0].to_s } : 0
     end
+  end
+
+  def compute_score
+    @tiles_count.each_pair.collect { |tile, count| tile[1] * count }.sum
   end
 
   public
@@ -55,6 +58,6 @@ if $PROGRAM_NAME == __FILE__
   puts Scrabble.new('street').score
   puts Scrabble.new('Humuhumunukunukuapuaa', Hawaiian_Tiles).score
   puts Scrabble.new('hello').score # 8
-  puts Scrabble.new('aloha', tile_set = hawaiian).score # 15
+  puts Scrabble.new('aloha', tiles = hawaiian).score # 15
   puts Scrabble.new("he‘e", hawaiian).score # => 14
 end
