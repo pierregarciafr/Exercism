8c0cd36efd86c4aeb612f7f34c7367527835ce3b
pierre garcia <pg.pierre.garcia@free.fr>
2021-07-27 00:29:27 +0200

    create Codon class for of_codon case

--- ruby/protein-translation/protein_translation.rb
+++ ruby/protein-translation/protein_translation.rb
@@ -3,69 +3,104 @@
 
 InvalidCodonError = Class.new(StandardError)
 
-module Translation
+class Codon
 
-  class << self
+  attr_reader :codon_acid
+  
+  CODONS = { 'AUG' => 'Methionine',
+              'UUU' => 'Phenylalanine', 'UUC' => 'Phenylalanine',
+             'UUA' => 'Leucine',       'UUG' => 'Leucine',
+             'UCU' => 'Serine',        'UCC' => 'Serine',
+             'UCA' => 'Serine',        'UCG' => 'Serine',
+             'UAU' => 'Tyrosine',      'UAC' => 'Tyrosine',
+             'UGU' => 'Cysteine',      'UGC' => 'Cysteine',
+             'UGG' => 'Tryptophan',
+             'UAA' => 'STOP',         'UAG' => 'STOP',         'UGA' => 'STOP'
+           }
 
-    CODONS = { 'AUG' => 'Methionine',
-               'UUU' => 'Phenylalanine', 'UUC' => 'Phenylalanine',
-               'UUA' => 'Leucine',       'UUG' => 'Leucine',
-               'UCU' => 'Serine',        'UCC' => 'Serine',
-               'UCA' => 'Serine',        'UCG' => 'Serine',
-               'UAU' => 'Tyrosine',      'UAC' => 'Tyrosine',
-               'UGU' => 'Cysteine',      'UGC' => 'Cysteine',
-               'UGG' => 'Tryptophan',
-               'UAA' => 'STOP',         'UAG' => 'STOP',         'UGA' => 'STOP'
-             }
+  def initialize(codon)
+    p "Codon.codon : #{codon}"
+    @codon_acid = CODONS[codon]
+  end
+end
+
+class Translation
 
     private
 
-    def ending_codon?(codon)
-      of_codon(codon) == 'STOP'
+    def initialize(codon)
+      @codon = codon
+      # @codon = codon.chars.each_slice(3).with_object([]) do |slice, result|
+      #   result << slice.join
+      # end
     end
 
-    def valid_codons?(strand_array)
-      valid_length?(strand_array) && valid_letters?(strand_array.join)
+    def ending_codon?
+      of_codon == 'STOP'
     end
 
-    def valid_length?(strand_array)
-      strand_array.length % 3 == 0
-    end
+    # def valid_codons?
+    #   valid_length?(@codon) && valid_letters?(@codon)
+    # end
 
-    def valid_letters?(strand)
-      strand.match(/[AUCG]*/)[0] == strand
-    end
+    # def valid_length?
+    #   @codon.length % 3 == 0
+    # end
 
-    def generate_result(strand_array)
-      strand_array.each_slice(3).with_object([]) do |slice, result|
-      	if ending_codon?(slice.join)
-      	  return result 
-      	else
-      	  result << of_codon(slice.join)
-        end
+    # def valid_letters?
+    #   @codon.join.match(/[AUCG]*/)[0] == strand
+    # end
+
+    def generate_result
+      p "codon: #{@codon}"
+      result = @codon.map do |codon_shortcut|
+      	# if ending_codon?(codon_shortcut)
+      	#   return result
+      	# else
+        p "codon_shortcut: #{codon_shortcut}"
+      	  Translation.of_codon(codon_shortcut)
+          
+        # end
       end
+      p "result : #{result}"
     end
 
     public
 
-    def of_codon(codon)
-      CODONS[codon]
+    def of_codon
+      p @codon
+      Codon.new(@codon).codon_acid
     end
 
-    def of_rna(strand)
-      strand_array = strand.chars
-      raise InvalidCodonError, "Incorrect codon" if !valid_codons?(strand_array)
-
-  	  generate_result(strand_array)
+    def self.of_codon(codon)
+      new(codon).of_codon
     end
 
-  end
+    # def of_rna
+    #   p @codon
+    #   # raise InvalidCodonError, "Incorrect codon" if !valid_codons?
+
+  	 #  generate_result
+    # end
+
+    # def self.of_rna(strand)
+    #   new(strand).of_rna
+    # end
+
 end
 
 if $PROGRAM_NAME == __FILE__
   # puts Translation.of_codon("UGG")
+  %w(UCU UCC UCA UCG).each do |codon|
+    p Translation.of_codon(codon) # assert_equal 'Serine',
+  end
+
+    # strand = 'AUGUUUUGG'
+    # expected = %w(Methionine Phenylalanine Tryptophan)
+    # p Translation.of_rna(strand)
+
   # p Translation.of_rna('CARROT') # 
-  p Translation.of_rna('AUGUUUUAA') # cas STOP
+  # p Translation.of_rna('AUGUUUUAA') # cas STOP
    # p Translation.of_rna('AUGUUUUGG') # cas until result.empty?
 
 end
