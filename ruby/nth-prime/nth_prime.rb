63d6974d77a03a3fdba705435008a3e8c6f2c291
pierre garcia <pg.pierre.garcia@free.fr>
2021-07-26 21:10:00 +0200

    change module for a class with instance variable

--- ruby/nth-prime/nth_prime.rb
+++ ruby/nth-prime/nth_prime.rb
@@ -1,19 +1,33 @@
 # frozen_string_literal: true
 
 # Neme nombre premier
-module Prime
-  class << self
-    def nth(int)
-      raise(ArgumentError) unless int.positive?
 
-      premiers = [2]
-      number = 2
-      until premiers.length == int
-        number += 1
-        multiples = premiers.select { |premier| (number % premier).zero? }
-        premiers << number if multiples.empty?
-      end
-      premiers.max
-    end
+class Prime
+
+  def initialize(integer)
+    @integer = integer
   end
+
+  public
+
+  def nth
+    raise(ArgumentError) unless @integer.positive?
+
+    premiers = [2]
+    number = 2
+    until premiers.length == @integer
+      number += 1
+      multiples = premiers.select { |premier| (number % premier).zero? }
+      premiers << number if multiples.empty?
+    end
+    premiers.max
+  end
+  
+  def self.nth(integer)
+    new(integer).nth
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+   p Prime.nth(10001) # 104743,
 end
