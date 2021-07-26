f5dce720619117c56af3f4f671e55a359044302d
pierre garcia <pg.pierre.garcia@free.fr>
2021-07-26 20:17:59 +0200

    rewrite module to class with instance variables

--- ruby/all-your-base/all_your_base.rb
+++ ruby/all-your-base/all_your_base.rb
@@ -1,51 +1,72 @@
 # frozen_string_literal: true
 
 # Conversion d' 'une base a l'autre
-module BaseConverter
-  class << self
-    def convert(input_base, digits, output_base)
-      valid_arguments?(input_base, digits, output_base)
-      result = convert_to_decimal(input_base, digits)
-      decimal_to_output(output_base, result)
-    end
+
+  class BaseConverter
 
     private
 
-    def valid_arguments?(input_base, digits, output_base)
-      raise ArgumentError if invalid_base?(input_base, output_base)
-      raise ArgumentError if invalid_digits?(digits, input_base)
+    def initialize(input_base, digits, output_base)
+      @input_base = input_base
+      @digits = digits
+      @output_base = output_base
     end
 
-    def convert_to_decimal(input_base, digits)
-      digits.reverse.map.with_index do |n, i|
-        n * (input_base**i)
+    def valid_arguments?
+      raise ArgumentError if invalid_base?
+      raise ArgumentError if invalid_digits?
+    end
+
+    def convert_to_decimal
+      @digits.reverse.map.with_index do |n, i|
+        n * (@input_base**i)
       end.sum
     end
 
-    def decimal_to_output(output_base, result)
+    def decimal_to_output(result)
       array = result.zero? ? [0] : []
 
       until result.zero?
-        array.unshift(result % output_base)
-        result /= output_base
+        array.unshift(result % @output_base)
+        result /= @output_base
       end
       array
     end
 
-    def invalid_base?(input_base, output_base)
-      input_base < 2 || output_base < 2
+    def invalid_base?
+      @input_base < 2 || @output_base < 2
     end
 
-    def invalid_digits?(digits, input_base)
-      negative_digit?(digits) || value_out_of_base?(digits, input_base)
+    def invalid_digits?
+      negative_digit? || value_out_of_base?
     end
 
-    def negative_digit?(digits)
-      !digits.select(&:negative?).empty?
+    def negative_digit?
+      !@digits.select(&:negative?).empty?
     end
 
-    def value_out_of_base?(digits, input_base)
-      !digits.select { |n| n >= input_base }.empty?
+    def value_out_of_base?
+      !@digits.select { |n| n >= @input_base }.empty?
     end
+
+    public
+
+    def self.convert(input_base, digits, output_base)
+      new(input_base, digits, output_base).convert
+    end
+
+    def convert
+      valid_arguments?
+      decimal_to_output(convert_to_decimal)
+    end
+
   end
+
+if $PROGRAM_NAME == __FILE__
+    digits = [1, 0, 1]
+    input_base = 2
+    output_base = 10
+    expected = [5]
+
+    p BaseConverter.convert(input_base, digits, output_base)
 end
