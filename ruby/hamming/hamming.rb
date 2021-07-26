d90c4af0ef964b41c1d49476a988926f13c38185
pierre garcia <pg.pierre.garcia@free.fr>
2021-07-26 20:44:03 +0200

    change module to class with instance variables

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,13 +1,25 @@
-module Hamming
-  class << self
-    def compute(string_1, string_2)
-      raise(ArgumentError) if string_1.length != string_2.length
+class Hamming
 
-      string_to_array(string_1).zip(string_to_array(string_2)).count { |s1, s2| s1 != s2 }
-    end
+  private
 
-    def string_to_array(string)
-      string.each_char.to_a
-    end
+  def initialize(string_1, string_2)
+    @string_1 = string_1
+    @string_2 = string_2
+  end
+
+  def string_to_array(string)
+    string.each_char.to_a
+  end
+
+  public
+
+  def self.compute(string_1, string_2)
+    new(string_1, string_2).compute
+  end
+
+  def compute
+    raise(ArgumentError) if @string_1.length != @string_2.length
+
+    string_to_array(@string_1).zip(string_to_array(@string_2)).count { |s1, s2| s1 != s2 }
   end
 end
