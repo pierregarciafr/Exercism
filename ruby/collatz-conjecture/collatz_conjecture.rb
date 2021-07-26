86fcd017f38c0f7e57d9664f45bff75b352efb2b
pierre garcia <pg.pierre.garcia@free.fr>
2021-07-26 20:38:18 +0200

    change module to class, with instance variable

--- ruby/collatz-conjecture/collatz_conjecture.rb
+++ ruby/collatz-conjecture/collatz_conjecture.rb
@@ -1,21 +1,38 @@
 # frozen_string_literal: true
 
 # module doc comment
-module CollatzConjecture
-  class << self
-    def steps(result)
-      raise(ArgumentError) unless n.positive?
 
-      count = 0
-      until result == 1
-        result = operate(result)
-        count += 1
-      end
-      count
-    end
+class CollatzConjecture
 
-    def operate(int)
-      int.even? ? int / 2 : 3 * int + 1
-    end
+  private
+
+  def initialize(start_value)
+    @start_value = start_value
   end
+
+  def operate(int)
+    int.even? ? int / 2 : 3 * int + 1
+  end
+
+  public
+
+  def self.steps(start_value)
+    new(start_value).steps
+  end
+
+  def steps
+    raise(ArgumentError) unless @start_value.positive?
+    
+    count = 0
+    value = @start_value
+    until value == 1
+      value = operate(value)
+      count += 1
+    end
+    count
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  p CollatzConjecture.steps(1_000_000)
 end
