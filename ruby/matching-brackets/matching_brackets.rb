b357c156418cc149ced7d98183c16623d1981f37
pierre garcia <pg.pierre.garcia@free.fr>
2021-07-26 20:53:03 +0200

    change module for class with instance variable

--- ruby/matching-brackets/matching_brackets.rb
+++ ruby/matching-brackets/matching_brackets.rb
@@ -1,36 +1,53 @@
 # frozen_string_literal: true
 
 # '{[]}' -> (identify '[]', extract '[]', then identify '{}')
-module Brackets
-  class << self
-    def paired?(string)
-      process_pair_recognize_and_extract(
-        remove_all_non_bracket_characters(string)
-      )
-    end
 
-    def process_pair_recognize_and_extract(expression)
-      until expression.empty?
 
-        pairs = recognize_complete_pairs(expression)
-        return false if pairs.empty?
+class Brackets
 
-        expression = extract_found_pairs_from_string(pairs, expression)
-      end
-      true
-    end
+  private
 
-    def recognize_complete_pairs(expression_to_be_scanned)
-      expression_to_be_scanned.scan(/(\[\])|(\{\})|(\(\))/).flatten.compact
-    end
-
-    def remove_all_non_bracket_characters(expr)
-      expr.scan(%r{[^\s\w\+\.\/\*\-\\\^\&]}).join
-    end
-
-    def extract_found_pairs_from_string(array, expression)
-      array.each { |pair| expression = expression.gsub(pair, '') }
-      expression
-    end
+  def initialize(string)
+    @string = string
   end
+
+  def process_pair_recognize_and_extract(expression)
+    until expression.empty?
+
+      pairs = recognize_complete_pairs(expression)
+      return false if pairs.empty?
+
+      expression = extract_found_pairs_from_string(pairs, expression)
+    end
+    true
+  end
+
+  def recognize_complete_pairs(expression_to_be_scanned)
+    expression_to_be_scanned.scan(/(\[\])|(\{\})|(\(\))/).flatten.compact
+  end
+
+  def remove_all_non_bracket_characters
+    @string.scan(%r{[^\s\w\+\.\/\*\-\\\^\&]}).join
+  end
+
+  def extract_found_pairs_from_string(array, expression)
+    array.each { |pair| expression = expression.gsub(pair, '') }
+    expression
+  end
+
+  public
+
+  def self.paired?(string)
+    new(string).paired?
+  end
+
+  def paired?
+    process_pair_recognize_and_extract(
+      remove_all_non_bracket_characters
+    )
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  p Brackets.paired?('[]')
 end
