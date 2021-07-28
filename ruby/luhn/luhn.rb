1b753732901f1f84be30dfa319f7437dedca3c83
pierre garcia <pg.pierre.garcia@free.fr>
2021-07-29 01:38:07 +0200

    change .valid? so that it is a real instance method, and change instance variables and the validation process methods

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,30 +1,56 @@
 class Luhn
 
+  REGULAR_EXPRESSION = {
+    digit_or_space: /[[:digit:]]|[[:blank:]]/,
+    digit: /[[:digit:]]/
+  }
+
+  attr_reader :validity
+
   private
 
   def initialize(card_number)
-    @card_number = card_number
+    @validity = card_number.nil? ? false : valid?(card_number)
+    # @validity = valid?(card_number)
+    # @status = @validity ? luhn_test(card_number) : false
+
   end
 
-  def single_digit?
-    filter_digits.size == 1
+  def only_digits?(card_number) # @card_number n'est que des chiffres ?
+    card_number.scan(REGULAR_EXPRESSION[:digit_or_space]) == card_number.chars 
   end
 
-  def unauthorized_character?
-    !@card_number.scan(/\D/).reject { |char| char == " " }.empty?
+  def filter_digits(card_number)
+    card_number.scan(REGULAR_EXPRESSION[:digit])
   end
 
-  def filter_digits
-    @card_number.scan(/[0-9]/)
+  def single_digit?(card_number)
+    filter_digits(card_number).size == 1
   end
 
-  public
+  # def valid?(card_number)
+  #   return nil unless only_digits?(card_number)
+  #   return nil if single_digit?(card_number)
+  # end
 
-  def valid?
-    return false if single_digit?
-    return false if unauthorized_character?
+  # def luhn_test(card_number)
 
-    filter_digits.reverse.each_with_index.map do |digit, index|
+  #     filter_digits(card_number).reverse.each_with_index.map do |digit, index|
+  #     if digit.to_i == 9
+  #       9
+  #     elsif index % 2 == 1
+  #       ((digit.to_i * 2) % 9)
+  #     else
+  #       digit.to_i
+  #     end
+  #   end
+  # end
+
+  def valid?(card_number)
+    return nil unless only_digits?(card_number)
+    return nil if single_digit?(card_number)
+
+    filter_digits(card_number).reverse.each_with_index.map do |digit, index|
       if digit.to_i == 9
         9
       elsif index % 2 == 1
@@ -34,10 +60,14 @@ class Luhn
       end
     end
                  .sum % 10 == 0
+    # p luhn_value
+    # luhn_value
   end
 
+  public
+
   def self.valid?(card_number)
-    new(card_number).valid?
+    new(card_number).validity # .card_number #.send(:valid?)
   end
 
 end
@@ -48,4 +78,5 @@ if $PROGRAM_NAME == __FILE__
   puts Luhn.valid?("0000 0") # true
   puts Luhn.valid?("095 245 88") # true
   puts Luhn.valid?("055-444 286") # false expected
+  puts Luhn.valid?('1')
 end
