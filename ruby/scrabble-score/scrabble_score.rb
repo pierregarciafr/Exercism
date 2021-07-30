4adc79d732357f64df6f36171876e247ebc2daa8
pierre garcia <pg.pierre.garcia@free.fr>
2021-07-30 22:18:46 +0200

    correct :upper: POSIX expression hooks

--- ruby/scrabble-score/scrabble_score.rb
+++ ruby/scrabble-score/scrabble_score.rb
@@ -1,17 +1,17 @@
 class Scrabble
   TILES = { A: 1, N: 1,
-           B: 3, O: 1,
-           C: 3, P: 3,
-           D: 2, Q: 10,
-           E: 1, R: 1,
-           F: 4, S: 1,
-           G: 2, T: 1,
-           H: 4, U: 1,
-           I: 1, V: 4,
-           J: 8, W: 4,
-           K: 5, X: 8,
-           L: 1, Y: 4,
-           M: 1, Z: 10 }
+            B: 3, O: 1,
+            C: 3, P: 3,
+            D: 2, Q: 10,
+            E: 1, R: 1,
+            F: 4, S: 1,
+            G: 2, T: 1,
+            H: 4, U: 1,
+            I: 1, V: 4,
+            J: 8, W: 4,
+            K: 5, X: 8,
+            L: 1, Y: 4,
+            M: 1, Z: 10 }
 
   def self.score(tiles)
     new(tiles).score
@@ -21,8 +21,10 @@ class Scrabble
 
   def initialize(letters_collection, tiles = TILES)
     @tiles_on_game = tiles.map { |tile| { letter: tile[0], score: tile[1], quantity: 0 } }
-    @letters_collection = letters_collection.to_s.upcase.scan(/[:upper:]/).sort!
-    @tiles = tiles
+    # p @tiles_on_game
+    @letters_collection = letters_collection.to_s.upcase.scan(/[[:upper:]]/).sort!
+    p @letters_collection
+    # @tiles = tiles
     normalize
     @score = compute_score
   end
