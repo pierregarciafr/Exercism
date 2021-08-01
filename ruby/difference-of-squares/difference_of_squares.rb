8920efc81da9182e94bf3b380985e0e34f17b1ee
pierre garcia <pg.pierre.garcia@free.fr>
2021-08-01 11:49:40 +0200

    turn instance variables into methods

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -1,10 +1,18 @@
 class Squares
-  attr_reader :number, :square_of_sum, :sum_of_squares, :difference
 
   def initialize(number)
     @number = number
-    @square_of_sum = [*1..number].sum**2
-    @sum_of_squares = [*1..number].collect { |x| x**2 }.sum
-    @difference = @square_of_sum - @sum_of_squares
+  end
+
+  def square_of_sum
+    [*1..@number].sum**2
+  end
+
+  def sum_of_squares
+    [*1..@number].sum { |x| x**2 }
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
   end
 end
